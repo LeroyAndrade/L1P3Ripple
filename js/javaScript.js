fcb1d7426e3d8fd88117ce70c6b53476c76ccffb
LeroyAndrade <30168@ma-web.nl>
2020-03-25 21:34:39 +0100

    Minor aanpassing

--- js/javaScript.js
+++ js/javaScript.js
@@ -6,17 +6,21 @@ rimpl.classList = 'rimpl';
 for(let i=0; i < alleVakken.length; i++){
   alleVakken[i].addEventListener('click', function(e){
     e.preventDefault();
-    let x = e.pageX - this.offsetLeft;
-    let y = e.pageY - this.offsetTop;
+    let x = e.pageX - this.offsetLeft -50;
+    let y = e.pageY - this.offsetTop -50;
     rimpl.style.left = x + 'px';
     rimpl.style.top  = y + 'px';
-
+e.target.append(rimpl);
     //het element oproepen
-    e.target.append(rimpl);
+    rimpl.classList.add('groeien');
 
     //
-    let dec = this.getElementsByTagName('input')[0];
-    //als
+
+    if(this.getElementsByTagName('input')[0]){
+      let dec = this.getElementsByTagName('input')[0];
         dec.checked = !dec.checked;
+    }
+    //als
+
   });
 }
